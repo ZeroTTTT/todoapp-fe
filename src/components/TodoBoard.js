7931d12a4f7d9ee94c057b839117f7d4a35f1dea
Youngho Park <dudgh4651@gmail.com>
2024-05-01 15:21:04 +0900

    first commit

---
+++ src/components/TodoBoard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoItem from './TodoItem'
+
+const TodoBoard = ({todoList, toggleComplete, deleteItem}) => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      {todoList.length>0?todoList.map((item, index) => (
+      <TodoItem 
+        item={item}
+        key={index}
+        deleteItem={deleteItem}
+        toggleComplete={toggleComplete} 
+        />)
+        ) : <h2>There is no Item to show</h2>}      
+    </div>
+  );
+};
+
+export default TodoBoard;
